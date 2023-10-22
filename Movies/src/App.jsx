a9dad7e6c676880cc755ad481cabf3599da1f08f
Lucas Mendes <lucas_08m@hotmail.com>
2023-10-22 01:39:38 -0300

    add request titles to API

--- Movies/src/App.jsx
+++ Movies/src/App.jsx
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      <h2>Movies</h2>
       <Outlet />
     </div>
   )
--- Movies/src/pages/Home.jsx
+++ Movies/src/pages/Home.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react"
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+
+  }
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+ 
+    getTopRatedMovies(topRatedUrl)
+
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className="container">
+      <h2 className="title">Melhores Filmes:</h2>
+      <div className="movies-container">
+      {topMovies.length === 0 && <p>Carregando...</p>}
+      {topMovies.length > 0 &&  topMovies.map((movie) =>
+        <p>{movie.title}</p>
+       )}
+      </div>
+    </div>
   )
 }
 
