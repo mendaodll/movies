1c7181b7a018a6d1dca1681eacef7bcbd9becb6c
Lucas Mendes <lucas_08m@hotmail.com>
2023-10-22 01:14:24 -0300

    adding first components n styling

---
+++ Movies/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+    <h2>
+      <Link to="/"><BiCameraMovie/>MoviesLib</Link>
+    </h2>
+    <form >
+        <input type="text" placeholder="Buscar" />
+        <button type="submit">
+            <BiSearchAlt2/>
+        </button>
+    </form>
+  </nav>
+  )
+}
+
+export default Navbar
--- Movies/src/main.jsx
+++ Movies/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Movie from './pages/Movie.jsx'
+import Search from './pages/Search.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route element={<App />}> 
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
