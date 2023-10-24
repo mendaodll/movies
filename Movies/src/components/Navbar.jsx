904ede02d8cd1ef343c671c71235903947975441
Lucas Mendes <lucas_08m@hotmail.com>
2023-10-24 01:13:51 -0300

    feat: add searching movies function

--- Movies/src/components/Navbar.jsx
+++ Movies/src/components/Navbar.jsx
@@ -1,17 +1,36 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
 
 import './Navbar.css'
 
 
 const Navbar = () => {
+
+  const[search, setSearch] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!search) return 
+
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  }
+
   return (
     <nav id="navbar">
     <h2>
       <Link to="/"><BiCameraMovie/>MoviesLib</Link>
     </h2>
-    <form >
-        <input type="text" placeholder="Buscar" />
+    <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="Buscar" 
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+        />
         <button type="submit">
             <BiSearchAlt2/>
         </button>
--- Movies/src/pages/Search.jsx
+++ Movies/src/pages/Search.jsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import MovieCard from "../components/MovieCard"
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
 
 const Search = () => {
+  const [searchParams] = useSearchParams()
+
+  const [movies, setMovies] = useState([])
+
+  const query = searchParams.get('q')
+
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setMovies(data.results)
+
+  }
+
+  useEffect(() => {
+
+    const searchWithQueryURL = `${searchURL}?query=${query}&${apiKey}&`
+ 
+    getSearchedMovies(searchWithQueryURL)
+
+  }, [query])
+
+
   return (
-    <div>Search</div>
+    <div className="container">
+    <h2 className="title">
+      Resultados para: <span className="query-text">{query}</span>
+      </h2>
+    <div className="movies-container">
+    {movies &&  
+      movies.map((movie) =>
+      <MovieCard key={movie.id}  movie={movie} />
+     )}
+    </div>
+  </div>
   )
 }
 
